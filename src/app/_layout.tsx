762e649e62d4a6f4304c9606b75ecbbb2652fa4d
Nelsokchhunly <sokchhunlynel@gmail.com>
2025-01-09 17:34:33 +0900

    feat: add gluestack for library components

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,16 @@
 import { Stack } from "expo-router";
 
+import "@/global.css";
+import { GluestackUIProvider } from "@src/components/ui/gluestack-ui-provider";
+
 export default function RootLayout() {
-  return <Stack
-    screenOptions={{
-      headerShown: false,
-    }}
-  />;
+  return (
+    <GluestackUIProvider mode="light">
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </GluestackUIProvider>
+  );
 }
